9854c2c9923826f543eafcd04faf0c4d9e1a7a9e
Rahul R <127472805+Rahul-R-1796@users.noreply.github.com>
2023-05-29 15:36:17 +0530

    Add files via upload

--- Library mangement/src/Data/data.js
+++ Library mangement/src/Data/data.js
@@ -9,28 +9,29 @@ export const data =[
     {
     id: "2",
     name: "Sapiens: A Brief History of Humankind",
-    image: "https://m.media-amazon.com/images/I/51a4L+B1bhL._SX331_BO1,204,203,200_.jpg",
+    image: "https://images.gr-assets.com/books/1420788020l/20839196.jpg",
     author: "Yuval Noah Harari",
     description: "A thought-provoking exploration of human history and the forces that shaped our species."
     },
     {
     id: "3",
     name: "The Alchemist",
-    image: "https://m.media-amazon.com/images/I/41d+9KSXsEL._SX323_BO1,204,203,200_.jpg",
+    image: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1654371463i/18144590.jpg",
     author: "Paulo Coelho",
     description: "A captivating story about following your dreams and the importance of self-discovery."
     },
     {
     id: "4",
     name: "The Great Gatsby",
-    image: "https://m.media-amazon.com/images/I/51B0ykxOQSL._SX313_BO1,204,203,200_.jpg",
+    image: "https://m.media-amazon.com/images/I/41JFqILaXyL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
     author: "F. Scott Fitzgerald",
     description: "An American classic that delves into themes of wealth, love, and the American Dream."
     },
         {
     id: "5",
     name: "To Kill a Mockingbird",
-    image: "https://m.media-amazon.com/images/I/51zmD+1VbTL._SX321_BO1,204,203,200_.jpg",
+    
+    image: "https://m.media-amazon.com/images/P/0099419785.01._SCLZZZZZZZ_SX500_.jpg",
     author: "Harper Lee",
     description: "A powerful novel addressing racial injustice and moral growth in the Deep South of the 1930s."
     }
